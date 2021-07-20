d4e2f9a0c0c7db3564340be0c55f0f3ef9711d0e
Abhiroop <abhiroop.m25902@gmail.com>
2021-07-20 11:27:57 +0530

    rename: util.py to file_reader.py

--- utils/email.py
+++ utils/email.py
@@ -5,7 +5,8 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-from utils import util
+from utils import file_reader
+
 
 def send_mail():
     # set up the SMTP server
@@ -13,12 +14,11 @@ def send_mail():
     s.ehlo()
     s.starttls()
 
-    credential = util.read_credential('login_credential.txt')
+    credential = file_reader.read_credential('login_credential.txt')
     s.login(credential[0], credential[1])
 
-    names, emails = util.get_contacts('contactList.txt')  # read contact
-    message_template = util.read_template('message.txt')
-
+    names, emails = file_reader.get_contacts('contactList.txt')  # read contact
+    message_template = file_reader.read_template('message.txt')
 
     # For each contact, send the email:
     for name, email in zip(names, emails):
@@ -28,14 +28,14 @@ def send_mail():
         message = message_template.substitute(PERSON_NAME=name.title())
 
         # setup the parameters of the message
-        msg['From']=credential[0]
-        msg['To']=email
-        msg['Subject']="This is TEST"
+        msg['From'] = credential[0]
+        msg['To'] = email
+        msg['Subject'] = "This is TEST"
 
         # add in the message body
         msg.attach(MIMEText(message, 'plain'))
 
         # send the message via the server set up earlier.
         s.send_message(msg)
-        
+
         del msg

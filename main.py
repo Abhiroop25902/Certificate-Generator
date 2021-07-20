993d0bc2c0869bad970ee5b7ccccbf443053831f
Abhiroop <abhiroop.m25902@gmail.com>
2021-07-20 11:58:49 +0530

    added email with image functionality

--- main.py
+++ main.py
@@ -1,14 +1,39 @@
 # Program to write a text on cert.png using Pillow
 #
+import smtplib
 from utils.image import generateCertImage
 from utils.email import send_mail
+from utils import file_reader
 
 EVENT = 'Portfolio-Contest'
 CERTIFICATE = './cert.png'
+LOGIN_CREDENTIAL_FILE = './login_credential.txt'
+CONTACT_LIST_FILE = 'contactList.txt'
+MESSSAGE_FORMAT_FILE = 'message.txt'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+
+# import the smtplib module. It should be included in Python by default
 
 
 if __name__ == '__main__':
     # Open image
     # generateCertImage(name='Arnab Sen', certificate=CERTIFICATE, event=EVENT)
-    send_mail()
+
+    # set up the SMTP server
+    s = smtplib.SMTP(host=EMAIL_HOST, port=EMAIL_PORT)
+    s.starttls()
+
+    credential = file_reader.read_credential(LOGIN_CREDENTIAL_FILE)
+    s.login(credential[0], credential[1])
+
+    names, dest_emails = file_reader.get_contacts(
+        CONTACT_LIST_FILE)  # read contact
+    message_template = file_reader.read_template(MESSSAGE_FORMAT_FILE)
+
+    # For each contact, send the email:
+    for name, dest_email in zip(names, dest_emails):
+        send_mail(s, message_template, name, dest_email,
+                  src_email=credential[0], image_file = CERTIFICATE)
     print("mail sent")
--- utils/email.py
+++ utils/email.py
@@ -1,41 +1,37 @@
-# import the smtplib module. It should be included in Python by default
-import smtplib
+
 
 # import necessary packages
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
 
-from utils import file_reader
+import smtplib
+from string import Template
+import os
 
 
-def send_mail():
-    # set up the SMTP server
-    s = smtplib.SMTP(host='smtp.gmail.com', port=587)
-    s.ehlo()
-    s.starttls()
+def send_mail(s: smtplib.SMTP, message_template: Template, name: str,
+              dest_email: str, src_email: str, image_file: str):
+    msg = MIMEMultipart()       # create a message
 
-    credential = file_reader.read_credential('login_credential.txt')
-    s.login(credential[0], credential[1])
+    # add in the actual person name to the message template
+    message = message_template.substitute(PERSON_NAME=name.title())
 
-    names, emails = file_reader.get_contacts('contactList.txt')  # read contact
-    message_template = file_reader.read_template('message.txt')
+    # setup the parameters of the message
+    msg['From'] = src_email
+    msg['To'] = dest_email
+    msg['Subject'] = "This is TEST"
 
-    # For each contact, send the email:
-    for name, email in zip(names, emails):
-        msg = MIMEMultipart()       # create a message
+    # add in the message body
+    msg.attach(MIMEText(message, 'plain'))
 
-        # add in the actual person name to the message template
-        message = message_template.substitute(PERSON_NAME=name.title())
+    with open(image_file, 'rb') as f:
+        img_data = f.read()
 
-        # setup the parameters of the message
-        msg['From'] = credential[0]
-        msg['To'] = email
-        msg['Subject'] = "This is TEST"
+    image = MIMEImage(img_data, name=os.path.basename(image_file))
+    msg.attach(image)
 
-        # add in the message body
-        msg.attach(MIMEText(message, 'plain'))
+    # send the message via the server set up earlier.
+    s.send_message(msg)
 
-        # send the message via the server set up earlier.
-        s.send_message(msg)
-
-        del msg
+    del msg
